a86c61a8c9cd0d2681bcdd83419d80351cd89eac
Ilya Benikov <ilya.benikov@gmail.com>
2023-01-25 14:41:23 +0400

    Created reusable cell

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Contacts
-//
-//  Created by илья on 23.01.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,3 +10,27 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "MyCell"){
+            print("Use old cell with index \(indexPath.row)")
+            cell = reuseCell
+        } else {
+            print("Create new cell with index \(indexPath.row)")
+            cell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+        }
+        configure(cell: &cell, for: indexPath)
+        return cell
+    }
+    
+    private func configure(cell: inout UITableViewCell, for indexPath: IndexPath){
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+    }
+}
